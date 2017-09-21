79ea7e181410a37e928c4de392f37714ad35d6d6
niebin0924 <804186194@qq.com>
2017-09-21 10:48:54 +0800

    ting

---
+++ src/components/Hello.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <router-link to="/about">about us</router-link>
+    </ul>
+
+    <div id="app-if">
+      <p v-if="seen">现在你看到我了</p>
+    </div>
+
+    <div id="app-for">
+      <ol>
+        <li v-for="todo in todos">
+          {{ todo.text }}
+        </li>
+      </ol>
+    </div>
+
+    <div id="app-on">
+      <p>{{message}}</p>
+      <button v-on:click="reverseMessage">逆转消息</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js',
+      seen: true,
+      todos: [
+        { text: '学习JavaScript' },
+        { text: '学习Vue' },
+        { text: '整个牛项目' }
+      ],
+      message: 'Hello Vue.js!'
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+}
+
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/about.vue
@@ -0,0 +1,46 @@
+
+
+<template>
+  <div class="about">
+    <h1>{{ msg }}</h1>
+    <h2>About Us</h2>
+    <about-one></about-one>
+
+  </div>
+</template>
+
+<script>
+  import AboutOne from './about_one.vue'
+  export default {
+    name: 'about',
+    data () {
+      return {
+        msg: '关于我们'
+      }
+    },
+    components: {
+      AboutOne
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
